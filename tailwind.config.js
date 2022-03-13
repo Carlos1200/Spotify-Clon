bf340f8ae158bb00cdc2030f50dda746aa36f8d6
Carlos1200 <carlos1200herrera@gmail.com>
2022-03-12 23:57:03 -0600

    Configurando eslint prettier husky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1db954',
+        'primary-black': '#212121',
+        'secondary-black': '#121212',
+        'primary-gray': '#535353',
+        'secondary-gray': '#b3b3b3',
+      },
+    },
   },
   plugins: [],
 }
